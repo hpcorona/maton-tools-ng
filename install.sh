80ea62af50688835201dc180055608ef4f0c7ae6
Hilario Pérez Corona <hpcorona@gmail.com>
2011-12-30 18:13:41 -0800

    * Install script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo "Copying libv8.dylib to /usr/lib/"
+sudo cp libv8.dylib /usr/lib/
+echo "Copying libv8wrap.so to /usr/lib/"
+sudo cp libv8wrap.so /usr/lib/
+echo "Copying ng to /usr/bin/"
+sudo cp ng /usr/bin/
+echo "Done."
+
